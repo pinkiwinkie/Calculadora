5d42fb60abd8eb0cccf51003914a01da60984e5f
pinkiwinkie <97902221+pinkiwinkie@users.noreply.github.com>
2023-04-01 12:10:33 +0200

    no se que pasa con el fondo del boton que no se pone en blanco

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickUno (View view){
+        TextView display = findViewById(R.id.bt1);
+        display.setText("1");
     }
 }
