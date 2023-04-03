94c11546ca525328f0efd9f821847c71b9faf602
pinkiwinkie <elrumor99@gmail.com>
2023-04-03 20:24:28 +0200

    ya pone dos numeros a la vez y quita el 0 inicial

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -14,8 +14,86 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void clickCero (View view) {
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("0");
+        else
+            display.setText(display.getText() + "0");
+    }
+
     public void clickUno (View view){
-        TextView display = findViewById(R.id.bt1);
-        display.setText("1");
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("1");
+        else
+            display.setText(display.getText() + "1");
+    }
+
+    public void clickDos (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("2");
+        else
+            display.setText(display.getText() + "2");
+    }
+    public void clickTres (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("3");
+        else
+            display.setText(display.getText() + "3");
+    }
+    public void clickCuatro (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("4");
+        else
+            display.setText(display.getText() + "4");
+    }
+    public void clickCinco (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("5");
+        else
+            display.setText(display.getText() + "5");
+    }
+    public void clickSeis (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("6");
+        else
+            display.setText(display.getText() + "6");
+    }
+    public void clickSiete (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("7");
+        else
+            display.setText(display.getText() + "7");
+    }
+    public void clickOCho (View view){
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("8");
+        else
+            display.setText(display.getText() + "8");
+    }
+    public void clickNueve (View view) {
+        TextView display = findViewById(R.id.tvBox);
+        if (display.getText().equals("0"))
+            display.setText("9");
+        else
+            display.setText(display.getText() + "9");
+    }
+
+    public void clickClear (View view) {
+        TextView display = findViewById(R.id.tvBox);
+        display.setText("0");
+    }
+
+    public void clickSuma(View view){
+        TextView display = findViewById(R.id.tvBox);
+        int num = Integer.parseInt(display.getText().toString());
     }
 }
